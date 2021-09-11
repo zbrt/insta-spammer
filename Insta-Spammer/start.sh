745cf389e39df90716ffafcb363c37e63c97d1ef
R3AP3ER <88580564+zbrt@users.noreply.github.com>
2021-09-11 00:48:16 -0400

    Add files via upload

---
+++ Insta-Spammer/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sleep 2
+lolcat ascii.py
+sleep 1.5
+
+echo "  CREATED BY ----> ZBRT
+        AUTHOR GITHUB -----> zbrt.github.io"
+
+echo " STARTING THE SCRIPT :} "
+sleep 2
+
+python3 hehe.py
+
